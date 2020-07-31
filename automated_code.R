1938be6b8540290d0db601822abb637d8b99b5e7
awilson12 <apfeifer@email.arizona.edu>
2020-07-31 16:26:01 -0700

    updating figure

--- automated_code.R
+++ automated_code.R
@@ -9,6 +9,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 if("truncdist" %in% rownames(installed.packages())==FALSE){install.packages("truncdist"); require(truncdist)}else{require(truncdist)}
 if("ggplot2" %in% rownames(installed.packages())==FALSE){install.packages("ggplot2"); require(ggplot2)}else{require(ggplot2)}
 if("ggpubr" %in% rownames(installed.packages())==FALSE){install.packages("ggpubr"); require(ggpubr)}else{require(ggpubr)}
+if("triangle" %in% rownames(installed.packages())==FALSE){install.packages("triangle"); require(triangle)}else{require(triangle)}
 
 #----------------------simulation code---------------------------------
 
@@ -19,7 +20,6 @@ suppressMessages(suppressWarnings(source("probability_of_illness estimation.R"))
 #-----------------------validation code--------------------------------
 
 suppressMessages(suppressWarnings(source("comparison_to_allegra.R")))
-suppressMessages(suppressWarnings(source("schoen_ashbolt_water.R")))
 
 #comparison to simulated shower from Allegra et al.
 schoen.ashbolt(10000,C.air=2.9e3,IR=7.5e-3)
@@ -36,4 +36,4 @@ ggplot(data)+geom_histogram(aes(x=Dose,y=..density..,group=model,fill=model),col
   geom_density(aes(x=Dose,group=model,fill=model),alpha=0.4)+
   scale_fill_manual(name="",values=c("#0066CC","#3300CC"))+
   scale_y_continuous(name="Density")+
-  scale_x_continuous(name="Deposited Dose")+theme_pubr()
+  scale_x_continuous(name="Deposited Dose",trans="log10")+theme_pubr()
