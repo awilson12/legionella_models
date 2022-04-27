a940428a798887c4c477dc19236c4fb153ad9df1
amwilson12 <47330626+awilson12@users.noreply.github.com>
2022-04-27 15:56:37 -0700

    Updating based on R1 feedback

--- App-1/final_model_for_app.R
+++ App-1/final_model_for_app.R
@@ -186,11 +186,11 @@ hamilton<-function(showerduration,C.water,type="Conventional",age=11,sex="Male")
   C.aer.5.conv<-rlnorm(iterations,19.5,0.35)
   
   #concentration of aerosols at diameter 6-10
-  C.aer.6.conv<-rlnorm(iterations,20,0.31)
-  C.aer.7.conv<-rlnorm(iterations,20,0.31)
-  C.aer.8.conv<-rlnorm(iterations,20,0.31)
-  C.aer.9.conv<-rlnorm(iterations,20,0.31)
-  C.aer.10.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.6.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.7.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.8.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.9.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.10.conv<-rlnorm(iterations,20,0.31)
   
   #WATER EFFICIENT
   #concentration of aerosols at diameter 1-2
@@ -202,13 +202,13 @@ hamilton<-function(showerduration,C.water,type="Conventional",age=11,sex="Male")
   C.aer.4.eff<-rlnorm(iterations,18.7,0.52)
   C.aer.5.eff<-rlnorm(iterations,18.7,0.52)
   #concentration of aerosols at diameter 6-10
-  C.aer.6.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.7.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.9.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.10.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.6.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.7.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.9.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.10.eff<-rlnorm(iterations,18.3,0.14)
   
   #deposition efficiencies
   DE.1<-runif(iterations,0.23,0.25)
@@ -216,11 +216,11 @@ hamilton<-function(showerduration,C.water,type="Conventional",age=11,sex="Male")
   DE.3<-runif(iterations,0.36,0.62)
   DE.4<-runif(iterations,0.29,0.61)
   DE.5<-runif(iterations,0.19,0.52)
-  DE.6<-runif(iterations,0.10,0.4)
-  DE.7<-runif(iterations,0.06,0.29)
-  DE.8<-runif(iterations,0.03,0.19)
-  DE.9<-runif(iterations,0.01,0.12)
-  DE.10<-runif(iterations,0.01,0.06)
+  #DE.6<-runif(iterations,0.10,0.4)
+  #DE.7<-runif(iterations,0.06,0.29)
+  #DE.8<-runif(iterations,0.03,0.19)
+  #DE.9<-runif(iterations,0.01,0.12)
+  #DE.10<-runif(iterations,0.01,0.06)
   
   #fraction aerosolized
   F.1<-.1750
@@ -228,11 +228,11 @@ hamilton<-function(showerduration,C.water,type="Conventional",age=11,sex="Male")
   F.3<-.1556
   F.4<-.0667
   F.5<-.0389
-  F.6<-.0250
-  F.7<-.0278
-  F.8<-.0500
-  F.9<-.0528
-  F.10<-.0389
+  #F.6<-.0250
+  #F.7<-.0278
+  #F.8<-.0500
+  #F.9<-.0528
+  #F.10<-.0389
   
   #volume of aerosol for size bin i
   V.1<-(4/3)*pi*(1*10^(-6)/2)^3
@@ -240,26 +240,34 @@ hamilton<-function(showerduration,C.water,type="Conventional",age=11,sex="Male")
   V.3<-(4/3)*pi*(3*10^(-6)/2)^3
   V.4<-(4/3)*pi*(4*10^(-6)/2)^3
   V.5<-(4/3)*pi*(5*10^(-6)/2)^3
-  V.6<-(4/3)*pi*(6*10^(-6)/2)^3
-  V.7<-(4/3)*pi*(7*10^(-6)/2)^3
-  V.8<-(4/3)*pi*(8*10^(-6)/2)^3
-  V.9<-(4/3)*pi*(9*10^(-6)/2)^3
-  V.10<-(4/3)*pi*(10*10^(-6)/2)^3
+  #V.6<-(4/3)*pi*(6*10^(-6)/2)^3
+  #V.7<-(4/3)*pi*(7*10^(-6)/2)^3
+  #V.8<-(4/3)*pi*(8*10^(-6)/2)^3
+  #V.9<-(4/3)*pi*(9*10^(-6)/2)^3
+  #V.10<-(4/3)*pi*(10*10^(-6)/2)^3
   
   #conventional
+  #part1.conv<-(C.aer.1.conv*V.1)+(C.aer.2.conv*V.2)+(C.aer.3.conv*V.3)+
+  #            (C.aer.4.conv*V.4)+(C.aer.5.conv*V.5)+(C.aer.6.conv*V.6)+
+  #            (C.aer.7.conv*V.7)+(C.aer.8.conv*V.8)+(C.aer.9.conv*V.9)+
+  #            (C.aer.10.conv*V.10)
+  
   part1.conv<-(C.aer.1.conv*V.1)+(C.aer.2.conv*V.2)+(C.aer.3.conv*V.3)+
-              (C.aer.4.conv*V.4)+(C.aer.5.conv*V.5)+(C.aer.6.conv*V.6)+
-              (C.aer.7.conv*V.7)+(C.aer.8.conv*V.8)+(C.aer.9.conv*V.9)+
-              (C.aer.10.conv*V.10)
+    (C.aer.4.conv*V.4)+(C.aer.5.conv*V.5)
   
   #efficient
-  part1.eff<-(C.aer.1.eff*V.1)+(C.aer.2.eff*V.2)+(C.aer.3.eff*V.3)+
-             (C.aer.4.eff*V.4)+(C.aer.5.eff*V.5)+(C.aer.6.eff*V.6)+
-             (C.aer.7.eff*V.7)+(C.aer.8.eff*V.8)+(C.aer.9.eff*V.9)+
-             (C.aer.10.eff*V.10)
+  #part1.eff<-(C.aer.1.eff*V.1)+(C.aer.2.eff*V.2)+(C.aer.3.eff*V.3)+
+  #           (C.aer.4.eff*V.4)+(C.aer.5.eff*V.5)+(C.aer.6.eff*V.6)+
+  #           (C.aer.7.eff*V.7)+(C.aer.8.eff*V.8)+(C.aer.9.eff*V.9)+
+  #           (C.aer.10.eff*V.10)
   
-  part2<-(F.1*DE.1)+(F.2*DE.2)+(F.3*DE.3)+(F.4*DE.4)+(F.5*DE.5)+
-              (F.6*DE.6)+(F.7*DE.7)+(F.8*DE.8)+(F.9*DE.9)+(F.10*DE.10)
+  part1.eff<-(C.aer.1.eff*V.1)+(C.aer.2.eff*V.2)+(C.aer.3.eff*V.3)+
+    (C.aer.4.eff*V.4)+(C.aer.5.eff*V.5)
+  
+  #part2<-(F.1*DE.1)+(F.2*DE.2)+(F.3*DE.3)+(F.4*DE.4)+(F.5*DE.5)+
+  #            (F.6*DE.6)+(F.7*DE.7)+(F.8*DE.8)+(F.9*DE.9)+(F.10*DE.10)
+  
+  part2<-(F.1*DE.1)+(F.2*DE.2)+(F.3*DE.3)+(F.4*DE.4)+(F.5*DE.5)
   
   
   #Dose (conventional fixture)
