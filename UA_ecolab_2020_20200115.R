453ca1a0d8243b7f25c21404f8dfb9c7cfb55d09
amwilson12 <47330626+awilson12@users.noreply.github.com>
2022-04-27 16:05:40 -0700

    updates based on R1 comments

--- UA_ecolab_2020_20200115.R
+++ UA_ecolab_2020_20200115.R
@@ -66,19 +66,19 @@ schoen.ashbolt<-function(iterations,showerduration,C.water){
   #---------aerosol size 5 - 6 um---------------
   
   #fraction of total aerosolized organisms in aeorosol size 5-6
-  F1.56<-(1-F1.15)*0.36
+  #F1.56<-(1-F1.15)*0.36
   #12% of aerosols other than 1-5
   
   #fraction of total aerosols of size range 5-6 deposited at the alveoli
-  F2.56<-rtriangle(iterations,a=0.1,c=0.65,b=1)
+  #F2.56<-rtriangle(iterations,a=0.1,c=0.65,b=1)
   
   #--------aerosol size 6 - 10 um----------------
   
   #fraction of total aerosolized organisms in aerosol size 6-10
-  F1.610<-(1-F1.15)*0.56
+  #F1.610<-(1-F1.15)*0.56
   
   #fraction of total aerosols of size range 6-10 deposited at the alveoli
-  F2.610<-rtriangle(iterations,a=0.01,c=0.1,b=1)
+  #F2.610<-rtriangle(iterations,a=0.01,c=0.1,b=1)
   
   ###########################################################
   
@@ -109,7 +109,7 @@ schoen.ashbolt<-function(iterations,showerduration,C.water){
   C.air<-C.water*PC
   
   #calculating deposited dose
-  DD<-C.air*V.air*((F1.15*F2.15)+(F1.56*F2.56)+(F1.610*F2.610))
+  DD<-C.air*V.air*(F1.15*F2.15)
   
   #calculating infection risk
   infection.risk<-1-exp(-k*(DD))
@@ -117,8 +117,7 @@ schoen.ashbolt<-function(iterations,showerduration,C.water){
   
   #saving inputs and outputs
   
-  model<<-data.frame(FR=FR,IR=IR,PC=PC,F1.15=F1.15,F2.15=F2.15,F1.56=F1.56,F2.56=F2.56,F1.610=F1.610,
-                     F2.610=F2.610,k=k,DD=DD,infection.risk=infection.risk)
+  model<<-data.frame(FR=FR,IR=IR,PC=PC,F1.15=F1.15,F2.15=F2.15,k=k,DD=DD,infection.risk=infection.risk)
   require(ggplot2)
   require(ggpubr)
   require(corrplot)
@@ -128,7 +127,7 @@ schoen.ashbolt<-function(iterations,showerduration,C.water){
   
   mydata.cor=cor(model,method=c("spearman"))
   spearman.cor<-mydata.cor[,12]
-  inputvar<-c("FR","IR","PC","F1.15","F2.15","F1.56","F2.56","F1.610","F2.610",
+  inputvar<-c("FR","IR","PC","F1.15","F2.15",
               "k","DD","Infection Risk")
   outputvar<-"Infection Risk"
   
@@ -230,11 +229,11 @@ hamilton<-function(iterations,showerduration,C.water){
   C.aer.5.conv<-rlnorm(iterations,19.5,0.35)
   
   #concentration of aerosols at diameter 6-10
-  C.aer.6.conv<-rlnorm(iterations,20,0.31)
-  C.aer.7.conv<-rlnorm(iterations,20,0.31)
-  C.aer.8.conv<-rlnorm(iterations,20,0.31)
-  C.aer.9.conv<-rlnorm(iterations,20,0.31)
-  C.aer.10.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.6.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.7.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.8.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.9.conv<-rlnorm(iterations,20,0.31)
+  #C.aer.10.conv<-rlnorm(iterations,20,0.31)
   
   #WATER EFFICIENT
   #concentration of aerosols at diameter 1-2
@@ -246,13 +245,13 @@ hamilton<-function(iterations,showerduration,C.water){
   C.aer.4.eff<-rlnorm(iterations,18.7,0.52)
   C.aer.5.eff<-rlnorm(iterations,18.7,0.52)
   #concentration of aerosols at diameter 6-10
-  C.aer.6.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.7.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.9.eff<-rlnorm(iterations,18.3,0.14)
-  C.aer.10.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.6.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.7.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.8.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.9.eff<-rlnorm(iterations,18.3,0.14)
+  #C.aer.10.eff<-rlnorm(iterations,18.3,0.14)
   
   #deposition efficiencies
   DE.1<-runif(iterations,0.23,0.25)
@@ -260,11 +259,11 @@ hamilton<-function(iterations,showerduration,C.water){
   DE.3<-runif(iterations,0.36,0.62)
   DE.4<-runif(iterations,0.29,0.61)
   DE.5<-runif(iterations,0.19,0.52)
-  DE.6<-runif(iterations,0.10,0.4)
-  DE.7<-runif(iterations,0.06,0.29)
-  DE.8<-runif(iterations,0.03,0.19)
-  DE.9<-runif(iterations,0.01,0.12)
-  DE.10<-runif(iterations,0.01,0.06)
+  #DE.6<-runif(iterations,0.10,0.4)
+  #DE.7<-runif(iterations,0.06,0.29)
+  #DE.8<-runif(iterations,0.03,0.19)
+  #DE.9<-runif(iterations,0.01,0.12)
+  #DE.10<-runif(iterations,0.01,0.06)
   
   #fraction aerosolized
   F.1<-.1750
@@ -272,11 +271,11 @@ hamilton<-function(iterations,showerduration,C.water){
   F.3<-.1556
   F.4<-.0667
   F.5<-.0389
-  F.6<-.0250
-  F.7<-.0278
-  F.8<-.0500
-  F.9<-.0528
-  F.10<-.0389
+  #F.6<-.0250
+  #F.7<-.0278
+  #F.8<-.0500
+  #F.9<-.0528
+  #F.10<-.0389
   
   #volume of aerosol for size bin i
   V.1<-(4/3)*pi*(1*10^(-6)/2)^3
@@ -284,27 +283,34 @@ hamilton<-function(iterations,showerduration,C.water){
   V.3<-(4/3)*pi*(3*10^(-6)/2)^3
   V.4<-(4/3)*pi*(4*10^(-6)/2)^3
   V.5<-(4/3)*pi*(5*10^(-6)/2)^3
-  V.6<-(4/3)*pi*(6*10^(-6)/2)^3
-  V.7<-(4/3)*pi*(7*10^(-6)/2)^3
-  V.8<-(4/3)*pi*(8*10^(-6)/2)^3
-  V.9<-(4/3)*pi*(9*10^(-6)/2)^3
-  V.10<-(4/3)*pi*(10*10^(-6)/2)^3
+  #V.6<-(4/3)*pi*(6*10^(-6)/2)^3
+  #V.7<-(4/3)*pi*(7*10^(-6)/2)^3
+  #V.8<-(4/3)*pi*(8*10^(-6)/2)^3
+  #V.9<-(4/3)*pi*(9*10^(-6)/2)^3
+  #V.10<-(4/3)*pi*(10*10^(-6)/2)^3
   
   #conventional
+  #part1.conv<-(C.aer.1.conv*V.1)+(C.aer.2.conv*V.2)+(C.aer.3.conv*V.3)+
+  #  (C.aer.4.conv*V.4)+(C.aer.5.conv*V.5)+(C.aer.6.conv*V.6)+
+  #  (C.aer.7.conv*V.7)+(C.aer.8.conv*V.8)+(C.aer.9.conv*V.9)+
+  #  (C.aer.10.conv*V.10)
+  
   part1.conv<-(C.aer.1.conv*V.1)+(C.aer.2.conv*V.2)+(C.aer.3.conv*V.3)+
-    (C.aer.4.conv*V.4)+(C.aer.5.conv*V.5)+(C.aer.6.conv*V.6)+
-    (C.aer.7.conv*V.7)+(C.aer.8.conv*V.8)+(C.aer.9.conv*V.9)+
-    (C.aer.10.conv*V.10)
+    (C.aer.4.conv*V.4)+(C.aer.5.conv*V.5)
   
   #efficient
+  #part1.eff<-(C.aer.1.eff*V.1)+(C.aer.2.eff*V.2)+(C.aer.3.eff*V.3)+
+  #  (C.aer.4.eff*V.4)+(C.aer.5.eff*V.5)+(C.aer.6.eff*V.6)+
+  #  (C.aer.7.eff*V.7)+(C.aer.8.eff*V.8)+(C.aer.9.eff*V.9)+
+  #  (C.aer.10.eff*V.10)
+  
   part1.eff<-(C.aer.1.eff*V.1)+(C.aer.2.eff*V.2)+(C.aer.3.eff*V.3)+
-    (C.aer.4.eff*V.4)+(C.aer.5.eff*V.5)+(C.aer.6.eff*V.6)+
-    (C.aer.7.eff*V.7)+(C.aer.8.eff*V.8)+(C.aer.9.eff*V.9)+
-    (C.aer.10.eff*V.10)
+    (C.aer.4.eff*V.4)+(C.aer.5.eff*V.5)
   
-  part2<-(F.1*DE.1)+(F.2*DE.2)+(F.3*DE.3)+(F.4*DE.4)+(F.5*DE.5)+
-    (F.6*DE.6)+(F.7*DE.7)+(F.8*DE.8)+(F.9*DE.9)+(F.10*DE.10)
+  #part2<-(F.1*DE.1)+(F.2*DE.2)+(F.3*DE.3)+(F.4*DE.4)+(F.5*DE.5)+
+  #  (F.6*DE.6)+(F.7*DE.7)+(F.8*DE.8)+(F.9*DE.9)+(F.10*DE.10)
   
+  part2<-(F.1*DE.1)+(F.2*DE.2)+(F.3*DE.3)+(F.4*DE.4)+(F.5*DE.5)
   
   #Dose (conventional fixture)
   Dose.fixture.conv<-C.leg*B*t.shower*part1.conv*part2
@@ -338,8 +344,7 @@ hamilton<-function(iterations,showerduration,C.water){
   mydata.cor.conv=cor(model.conv,method=c("spearman"))
   spearman.conv<-mydata.cor.conv[,24]
   inputvar<-c("B","C.aer.1.conv","C.aer.2.conv","C.aer.3.conv","C.aer.4.conv","C.aer.5.conv",
-              "C.aer.6.conv","C.aer.7.conv","C.aer.8.conv","C.aer.9.conv","C.aer.10.conv","DE.1",
-              "DE.2","DE.3","DE.4","DE.5","DE.6","DE.7","DE.8","DE.9","DE.10","r","Dose.fixture.conv",
+              "DE.1","DE.2","DE.3","DE.4","DE.5","k","Dose.fixture.conv",
               "P.infect.conv")
   outputvar<-"Infection Risk"
   
@@ -364,8 +369,7 @@ hamilton<-function(iterations,showerduration,C.water){
   mydata.cor.eff=cor(model.eff,method=c("spearman"))
   spearman.eff<-mydata.cor.eff[,24]
   inputvar<-c("B","C.aer.1.eff","C.aer.2.eff","C.aer.3.eff","C.aer.4.eff","C.aer.5.eff",
-              "C.aer.6.eff","C.aer.7.eff","C.aer.8.eff","C.aer.9.eff","C.aer.10.eff","DE.1",
-              "DE.2","DE.3","DE.4","DE.5","DE.6","DE.7","DE.8","DE.9","DE.10","r","Dose.fixture.eff",
+              "DE.1","DE.2","DE.3","DE.4","DE.5","k","Dose.fixture.eff",
               "P.infect.eff")
   outputvar<-"Infection Risk"
   
--- schoen_ashbolt_water.R
+++ schoen_ashbolt_water.R
@@ -1,5 +1,6 @@
 
 schoen.ashbolt.water<-function(iterations,C.water,IR,FL){
+  require(triangle)
   
   #model function inputs
   #1-iterations
@@ -10,7 +11,7 @@ schoen.ashbolt.water<-function(iterations,C.water,IR,FL){
   #-----------model input parameters--------------------------
   
   #partitioning coefficient
-  PC<-runif(iterations,10^-6,1.9e-05)
+  PC<-rtriangle(iterations,a=1E-5,c=1E-4,b=1.9e-05)
   #min = low value
   #max = (Best estimate value - low value) + Best estimate value
   
@@ -19,44 +20,43 @@ schoen.ashbolt.water<-function(iterations,C.water,IR,FL){
   #----------aerosol size 1 - 5 um--------------
   
   #fraction of total aerosolized organisms in aeorosol size 1-5
-  F1.15<-runif(iterations,0.75,1)
+  F1.15<-rtriangle(iterations,a=0.5,b=1,c=0.75)
   #min = best estimate value
   #max = high value
   
   #fraction of total aerosols of size range 1-5um deposited at the alveoli
-  F2.15<-runif(iterations,0.2,0.54)
+  F2.15<-rtriangle(iterations,a=0,b=0.54,c=0.2)
   #min = best estimate value
   #max = high value
   
   #---------aerosol size 5 - 6 um---------------
   
   #fraction of total aerosolized organisms in aeorosol size 5-6
-  F1.56<-(1-F1.15)*0.36
+  #F1.56<-(1-F1.15)*0.36
   #12% of aerosols other than 1-5
   
   #fraction of total aerosols of size range 5-6 deposited at the alveoli
-  F2.56<-(1-F2.15)*0.125
+  #F2.56<-(1-F2.15)*0.125
   
   #--------aerosol size 6 - 10 um----------------
   
   #fraction of total aerosolized organisms in aerosol size 6-10
-  F1.610<-(1-F1.15)*0.56
+  #F1.610<-(1-F1.15)*0.56
   
   #fraction of total aerosols of size range 6-10 deposited at the alveoli
-  F2.610<-(1-F2.15)*0.0125
+  #F2.610<-(1-F2.15)*0.0125
   
   ###########################################################
   
   C.air<-C.water*PC
   
   #calculating deposited dose
-  DD<-C.air*IR*((F1.15*F2.15)+(F1.56*F2.56)+(F1.610*F2.610))
+  DD<-C.air*IR*(F1.15*F2.15)
   
   
   #saving inputs and outputs
   
-  model2<<-data.frame(IR=IR,PC=PC,F1.15=F1.15,F2.15=F2.15,F1.56=F1.56,F2.56=F2.56,F1.610=F1.610,
-                     F2.610=F2.610,DD=DD)
+  model2<<-data.frame(IR=IR,PC=PC,F1.15=F1.15,F2.15=F2.15,DD=DD)
   
   
 }
